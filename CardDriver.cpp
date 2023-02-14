667d586982c966dac1e2815254c5b8e2308d5068
JadElKassis <124092146+JadElKassis@users.noreply.github.com>
2023-02-14 02:16:55 -0500

    Update CardDriver.cpp

--- CardDriver.cpp
+++ CardDriver.cpp
@@ -1,6 +1,7 @@
 #include "Card.cpp"
-#include <iostream>
 
+#include <iostream>
+using namespace std;
 int main() {
 	Deck* deck = new Deck();
 	Hand* hand = new Hand();
@@ -8,6 +9,8 @@ int main() {
 	for (int i = 0; i < 5; i++) {
 		hand->addCard(deck->draw());
 	}
+	cout << "Cards in Hand: " << endl;
+	
 	hand->printHand();
 	int count = 0;
 	cout << "Remaining cards in deck: ";
@@ -15,9 +18,10 @@ int main() {
 		cout << deck->cards[i]->type << " ";
 		count++;
 	}
-	cout << count << endl;
+	cout << count << " cards left in the Deck" << endl;
 	count = 0;
 	hand->cards[2]->play(hand,deck);
+	cout << "Cards in Hand: " << endl;
 	hand->printHand();
 
 	cout << "Remaining cards in deck: ";
@@ -25,7 +29,7 @@ int main() {
 		cout << deck->cards[i]->type << " ";
 		count++;
 	}
-	cout << count << endl;
+	cout << count << " cards left in the deck" << endl;
 	//Player* player = new Player(hand);
 
 	return 0;
