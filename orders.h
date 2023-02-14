b92899a465ac29a2353415bd3944e607dca4f2b2
jtirona <99438020+jtirona@users.noreply.github.com>
2023-02-13 23:01:37 -0500

    Add files via upload

--- orders.h
+++ orders.h
@@ -1,69 +1,134 @@
-#include <iostream>
-#include <vector>
-#include <string>
+#include<iostream>
+#include<vector> 
+#include<iomanip> // To format output.
+#include<string> // To store the label of an order within the order object itself.
 using namespace std;
 
+//abstract order class
 class Order
 {
-public :
-	Order();
-private :
-}
-
-class Deploy : public Order 
-{
 public:
-	DeployOrder();
-private:
+    virtual ~Order() = 0;
+    virtual bool validate() const = 0;
+    virtual void execute() const = 0;
+    virtual string getType() const = 0;
+    virtual ostream& printOrder(ostream&) const = 0;
 
+private:
+    virtual Order* clone() const = 0;
+    friend ostream& operator<<(ostream&, const Order&);
+    friend class OrdersList;
+    
 };
 
-class Advance : public Order {
-public:
-	Advance();
-
-private:
-};
-
-class Bomb : public Order 
+class Deploy : public Order
 {
 public:
-	Bomb();
+    ~Deploy();
+    string getType() const override;
+    bool validate() const override;
+    void execute() const override;
 
 private:
+    const static string type;
+    Order* clone() const override;
+    ostream& printOrder(ostream&) const override;
+};
+
+
+class Advance : public Order
+{
+public:
+    ~Advance();
+    string getType() const override;
+    bool validate() const override;
+    void execute() const override;
+
+private:
+    const static string type;
+    Order* clone() const override;
+    ostream& printOrder(ostream&) const override;
+};
+
+class Bomb : public Order
+{
+public:
+    ~Bomb();
+    string getType() const override;
+    bool validate() const override;
+    void execute() const override;
+
+private:
+    const static string type;
+    Order* clone() const override;
+    ostream& printOrder(ostream&) const override;
 };
 
 class Blockade : public Order
 {
 public:
-	Blockade();
+    ~Blockade();
+    string getType() const override;
+    bool validate() const override;
+    void execute() const override;
+
 private:
+    const static string type;
+    Order* clone() const override;
+    ostream& printOrder(ostream&) const override;
 };
 
 class Airlift : public Order
 {
 public:
-	Airlift();
+    string getType() const override;
+    bool validate() const override;
+    void execute() const override;
+    ~Airlift();
+
 private:
+    const static string type;
+    Order* clone() const override;
+    ostream& printOrder(ostream&) const override;
 };
 
 class Negotiate : public Order
 {
 public:
-	Negotiate();
+    ~Negotiate();
+    string getType() const override;
+    bool validate() const override;
+    void execute() const override;
 
 private:
+    const static string type;
+    Order* clone() const override;
+    ostream& printOrder(ostream&) const override;
 };
 
-class OrderList
+class newOrder
 {
 public:
-  OrderList();
-  ~OrderList();
-  void addOrder(Order* order);
-  void removeOrder(Order* order);
-  void moveOrder(Order* order, int new_index);
-  void printOrder();
-private:
-  vector<Order*> vec_order_list;
+    Order* createOrder(string) const;
 };
+
+class OrdersList
+{
+public:
+    OrdersList();
+    ~OrdersList();
+    OrdersList(const OrdersList&);
+    void addOrder(Order* o);
+    void remove(int);
+    void move(int, int);
+    void executeOrders();
+    OrdersList& operator=(const OrdersList&);
+    vector<Order*> getOrders() const { return orders; }
+
+private:
+    vector<Order*> orders;
+
+    friend ostream& operator<<(ostream&, const OrdersList&);
+};
+
+void printOrders(const vector<Order*> &order_list);
