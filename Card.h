ba7c9aa11827151269e4e8cd5f1cc67a54db453c
JadElKassis <124092146+JadElKassis@users.noreply.github.com>
2023-02-14 02:16:21 -0500

    Update Card.h

--- Card.h
+++ Card.h
@@ -4,7 +4,7 @@
 #include <string>
 #include <cstdlib>
 #include <ctime>
-#include "Player.cpp"
+
 using namespace std;
 
 class Card;
@@ -18,10 +18,11 @@ private:
 public:
     string type;
     
-    Card(std::string v) : type(v) {}
-
+    Card(std::string v) : type(v) {};
+    Card(const Card& c);
     ~Card();
 
+    Card& operator=(const Card& c);
     
     void play(Hand* hand, Deck* deck);
     // bool removeFromHand(Hand* hand, Deck* deck);
@@ -38,7 +39,7 @@ public:
     Deck(const Deck& d);
     ~Deck();
     Card* draw();
-    //Card* operator[](int index) const { return deck[index]; }
+    Deck& operator=(const Deck& d);
 };
 
 class Hand {
@@ -57,11 +58,7 @@ public:
 
     void addCard(Card* c);
     void printHand();
-    //ostream& printOrder(ostream& out) const
-
-
-
-    // Hand::Hand& operator=(const Hand& h);
+    Hand& operator=(const Hand& h);
 };
 
 #endif
