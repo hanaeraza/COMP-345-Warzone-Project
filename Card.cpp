df5fa071030dcb599319a4eefb7b683a7cf3ee45
JadElKassis <124092146+JadElKassis@users.noreply.github.com>
2023-03-21 02:22:05 -0500

    Update Card.cpp

--- Card.cpp
+++ Card.cpp
@@ -124,10 +124,12 @@ Deck& Deck::operator=(const Deck& d) {
     }
     return *this;
 }
-void Card::play(Hand* hand, Deck *deck,Player* player) {
+
+void Card::play(Hand* hand, Deck *deck) {
 	newOrder orderCreate;  //
     OrdersList order_list; //creates an order list
-    player->order_list.addOrder(orderCreate.createOrder(this->type)); //creates an order and adds it to the order list
+    //player->
+    order_list.addOrder(orderCreate.createOrder(this->type)); //creates an order and adds it to the order list
 	
 	printOrders(order_list.getOrders());
 
@@ -169,4 +171,3 @@ ostream& operator<<(ostream& out, const Hand& h) {
 }
 
 #endif
-
