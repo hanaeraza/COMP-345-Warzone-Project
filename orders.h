69518899591af74c33c326fc0043775178d4c107
JadElKassis <124092146+JadElKassis@users.noreply.github.com>
2023-03-21 10:06:37 -0500

    Update orders.h

--- orders.h
+++ orders.h
@@ -5,106 +5,134 @@
 #include<string> 
 using namespace std;
 
+class Player;
+class Territory;
+
 //abstract order class, can be overritten by subclasses
 class Order
 {
 public:
+    Order();
+    explicit Order(Player &);
     virtual ~Order() = 0;
     virtual bool validate() const = 0;
-    virtual void execute() const = 0;
+    virtual void execute() = 0; //pure virtual method
     virtual string getType() const = 0;
     virtual ostream& printOrder(ostream&) const = 0;
+    Player *currentPlayer;
 
 private:
     virtual Order* clone() const = 0;
     friend ostream& operator<<(ostream&, const Order&);
     friend class OrdersList;
-    
 };
 
 class Deploy : public Order
 {
 public:
+    Deploy();
+    Deploy(Territory &, Player *, int); // Target territory, current player, amount
     ~Deploy(); //destructor
     string getType() const override; //returns order type
     bool validate() const override; //returns true if valid
-    void execute() const override; //executes if valid
+    virtual void execute(); //executes if valid
 
 private:
     const static string type;
     Order* clone() const override; //copy constructor
     ostream& printOrder(ostream&) const override; //prints order
+    Territory *target; // Target territory to deploy at
+    int *amount; // Amount of armies to deploy
 };
 
 
 class Advance : public Order
 {
 public:
+    Advance();
+    Advance(Territory &, Territory &, Player *, int); // Src, dest, current player, amount
     ~Advance();
     string getType() const override;
     bool validate() const override;
-    void execute() const override;
+    virtual void execute();
 
 private:
     const static string type;
     Order* clone() const override;
     ostream& printOrder(ostream&) const override;
+    Territory *source;
+    Territory *target;
+    int *amount;
 };
 
 class Bomb : public Order
 {
 public:
+    Bomb();
+    Bomb(Territory &, Player *); // Target territory to bomb, current player
     ~Bomb();
     string getType() const override;
     bool validate() const override;
-    void execute() const override;
+    virtual void execute();
 
 private:
     const static string type;
     Order* clone() const override;
     ostream& printOrder(ostream&) const override;
+    Territory *target;
 };
 
 class Blockade : public Order
 {
 public:
+    Blockade();
+    Blockade(Territory &, Player *); // Target, current player
     ~Blockade();
     string getType() const override;
     bool validate() const override;
-    void execute() const override;
+    virtual void execute();
 
 private:
     const static string type;
     Order* clone() const override;
     ostream& printOrder(ostream&) const override;
+    Territory *target;
 };
 
 class Airlift : public Order
 {
 public:
+    Airlift();
+    Airlift(Territory &, Territory &, Player *, int); // Source, target, current player, amount
+    ~Airlift();
     string getType() const override;
     bool validate() const override;
-    void execute() const override;
-    ~Airlift();
+    virtual void execute();
 
 private:
     const static string type;
     Order* clone() const override;
     ostream& printOrder(ostream&) const override;
+    Territory *source;
+    Territory *target;
+    int *amount;
 };
 
 class Negotiate : public Order
 {
 public:
+    Negotiate();
+    Negotiate(Player *, Player *);
     ~Negotiate();
     string getType() const override;
     bool validate() const override;
-    void execute() const override;
+    virtual void execute();
 
 private:
     const static string type;
     Order* clone() const override;
     ostream& printOrder(ostream&) const override;
+    Player *targetPlayer;
 };
 
 //creates an order object from a string
@@ -133,3 +161,4 @@ private:
 };
 
 void printOrders(const vector<Order*> &order_list);
+
