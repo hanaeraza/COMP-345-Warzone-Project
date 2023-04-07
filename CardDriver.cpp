ffa067d2e3766087a1522663087c96bdf459d7d2
JadElKassis <124092146+JadElKassis@users.noreply.github.com>
2023-04-07 14:03:40 -0500

    Update CardDriver.cpp

--- CardDriver.cpp
+++ CardDriver.cpp
@@ -1,36 +1,15 @@
-#include "Card.cpp"
+#include "GameEngine.h"
 
+#include <string>
 #include <iostream>
 using namespace std;
 int main() {
-	Deck* deck = new Deck();
-	Hand* hand = new Hand();
-	//Player* Player = new Player();
-	for (int i = 0; i < 5; i++) {
-		hand->addCard(deck->draw());
-	}
-	cout << "Cards in Hand: " << endl;
 	
-	hand->printHand();
-	int count = 0;
-	cout << "Remaining cards in deck: ";
-	for (int i = 0; i < deck->size; i++) {
-		cout << deck->cards[i]->type << " ";
-		count++;
-	}
-	cout << count << " cards left in the Deck" << endl;
-	count = 0;
-	hand->cards[2]->play(hand,deck);
-	cout << "Cards in Hand: " << endl;
-	hand->printHand();
+	GameEngine GameEngine;
 
-	cout << "Remaining cards in deck: ";
-	for (int i = 0; i < deck->size; i++) {
-		cout << deck->cards[i]->type << " ";
-		count++;
-	}
-	cout << count << " cards left in the deck" << endl;
-	//Player* player = new Player(hand);
-
+    // Run until end state is reached
+    while(GameEngine.getCurrentState() != "EndState") {
+    GameEngine.update(); 
+    }
 	return 0;
 }
